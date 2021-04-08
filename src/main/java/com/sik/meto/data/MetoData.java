b8b327548d5ee1886658a52dc267ab6a4425bd77
Si Kusners <simon.kusners@gmail.com>
2021-04-08 18:56:00 +0100

    Sort excel sheet tabs alphabetical

--- src/main/java/com/sik/meto/data/MetoData.java
+++ src/main/java/com/sik/meto/data/MetoData.java
@@ -1,6 +1,8 @@
 package com.sik.meto.data;
 
 import java.io.IOException;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -19,14 +21,22 @@ public class MetoData {
         Set<MonthlyWeatherData> locationSpecificData = manager.filterByStation(weatherData, "Paisley");
 
         Map<String,Set<MonthlyWeatherData>> dataByLocation = weatherData.stream()
+                .sorted()
                 .collect(Collectors.groupingBy(MonthlyWeatherData::getStationName,Collectors.toSet()));
+
+        LinkedHashMap<String, Set<MonthlyWeatherData>> sortedMap = new LinkedHashMap<>();
+
+        dataByLocation.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByKey(Comparator.naturalOrder()))
+                .forEachOrdered(x -> sortedMap.put(x.getKey(), x.getValue()));
 
         //reporter.printLocations(dataByLocation);
 
         //reporter.printRecordsAndSummary(weatherData);
         //reporter.printYearlyAverages(manager.getYearlyAverageWeatherDataMap());
 
-        excelWriter.writeWorkbook(dataByLocation);
+        excelWriter.writeHistoricWorkbook(sortedMap);
 
 //        sheffData.stream()
 //                .sorted()
--- src/main/java/com/sik/meto/data/MetoExcelWriter.java
+++ src/main/java/com/sik/meto/data/MetoExcelWriter.java
@@ -26,109 +26,21 @@ public class MetoExcelWriter {
     private FileWriter mainWriter;
     private FileWriter locationWriter;
     private FileWriter averageWriter;
-    private String excelFileName;
-    private FileOutputStream excelOut;
+    private String historicFileName;
+    private FileOutputStream historicOut;
+    private String summaryFileName;
+    private FileOutputStream summaryOut;
 
 
     public MetoExcelWriter() throws IOException {
         //this.extremes = new WeatherExtremesData();
         this.mainWriter = new FileWriter("/Users/sik/met-office/zz-metoffice-full.txt");
         this.averageWriter = new FileWriter("/Users/sik/met-office/metoffice-averages-extremes.txt");
-        this.excelFileName = "/Users/sik/met-office/MetOfficeHistoricData.xlsx";
-        this.excelOut = new FileOutputStream(this.excelFileName);
+        this.historicFileName = "/Users/sik/met-office/MetOfficeHistoricData.xlsx";
+        this.historicOut = new FileOutputStream(this.historicFileName);
     }
 
-//    public void printRecordsAndSummary(Set<MonthlyWeatherData> weatherData) throws IOException {
-//        this.printRecordHeadings();
-//
-//        weatherData.stream()
-//                .sorted()
-//                .collect(Collectors.toCollection(LinkedHashSet::new))
-//                .forEach(this::printRecord);
-//
-//        mainWriter.close();
-//    }
-
-    public void printLocations(Map<String, Set<MonthlyWeatherData>> locationData) throws IOException {
-        locationData.entrySet().forEach(this::printLocation);
-    }
-
-
-    private void printLocation(Map.Entry<String,Set<MonthlyWeatherData>> locationData) {
-        try {
-            String fileName = "/Users/sik/met-office/metoffice-"
-                    + locationData.getValue().stream().findFirst().get().getStationName()+ ".txt";
-            locationWriter = new FileWriter(fileName);
-            this.printRecordHeadings(locationWriter);
-            locationData.getValue().stream()
-                    .sorted()
-                    .collect(Collectors.toCollection(LinkedHashSet::new))
-                    .forEach(r -> this.printRecord(r,locationWriter));
-            locationWriter.close();
-            System.out.println("Report written to: " + fileName);
-        } catch(IOException ioe) {
-            ioe.printStackTrace();
-        }
-    }
-
-    public void printRecordHeadings(FileWriter writer) throws IOException {
-        writeLine(String.format(MONTH_DATA_FORMAT, "Station", "Month", "Min.Temp", "Max.Temp", "FrostDays", "RainMM", "SunHours"),writer);
-        writeLine(String.format(MONTH_DATA_FORMAT, "=======", "=====", "========", "========", "=========", "======", "========"),writer);
-    }
-
-    public void printRecord(MonthlyWeatherData monthData, FileWriter writer) {
-        try {
-            writeLine(String.format(MONTH_DATA_FORMAT, monthData.getStationName(),
-                    monthData.getMonthStartDate().format(YYYY_MM),
-                    monthData.getTempMinC(),
-                    monthData.getTempMaxC(),
-                    monthData.getAfDays(),
-                    monthData.getRainfallMm(),
-                    monthData.getSunHours()),writer);
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        }
-    }
-
-    public void printExtremes(WeatherExtremesData extremes, FileWriter writer) throws IOException {
-        writeLine("", writer);
-        writeLine(" Lowest Min.Temp C: " + extremes.getMinTemp() + " (" + extremes.getMinTempLocTime() + ")", writer);
-        writeLine("Highest Max.Temp C: " + extremes.getMaxTemp() + " (" + extremes.getMaxTempLocTime() + ")", writer);
-        writeLine("Highest AF Days   : " + extremes.getMaxAfDays() + " (" + extremes.getMaxAfDaysLocTime() + ")", writer);
-        writeLine("Max. Rainfall Mm  : " + extremes.getMaxRainfallMm() + " (" + extremes.getMaxRainfallMmLocTime() + ")", writer);
-        writeLine("Max. Sun Hours    : " + extremes.getMaxSunHours() + " (" + extremes.getMaxSunHoursLocTime() + ")", writer);
-        writeLine("", writer);
-    }
-
-    public void printYearlyAverages(Map<Integer,YearlyAverageWeatherData> yearlyAverageWeatherDataMap) throws IOException {
-        writeLine("", averageWriter);
-        writeLine(String.format(MONTH_DATA_FORMAT, "", "Year", "Min.Temp", "Max.Temp", "FrostDays", "RainMM", "SunHours"), averageWriter);
-        writeLine(String.format(MONTH_DATA_FORMAT, "", "====", "========", "========", "=========", "======", "========"), averageWriter);
-
-        for (Integer year: yearlyAverageWeatherDataMap.keySet()) {
-            YearlyAverageWeatherData yearData = yearlyAverageWeatherDataMap.get(year);
-            writeLine(String.format(MONTH_DATA_FORMAT, "",
-                    yearData.getYearStartDate().getYear(),
-                    DF.format(yearData.getAvgTempMinC()),
-                    DF.format(yearData.getAvgTempMaxC()),
-                    DF.format(yearData.getAvgAfDays()),
-                    DF.format(yearData.getAvgRainfallMm()),
-                    DF.format(yearData.getAvgSunHours())), averageWriter);
-        }
-
-        //printExtremes(averageWriter);
-        averageWriter.close();
-    }
-
-    private void writeLine(String line, FileWriter writer) throws IOException {
-        writer.write(line + CR);
-    }
-
-    private void writeAverageLine(String averageLine, FileWriter writer) throws IOException {
-        writer.write(averageLine + CR);
-    }
-
-    public void writeWorkbook(Map<String, Set<MonthlyWeatherData>> locationData) throws IOException {
+    public void writeHistoricWorkbook(Map<String, Set<MonthlyWeatherData>> locationData) throws IOException {
         HSSFWorkbook workbook = new HSSFWorkbook();
 
         for(String location: locationData.keySet()) {
@@ -149,16 +61,16 @@ public class MetoExcelWriter {
                     .collect(Collectors.toCollection(LinkedHashSet::new))
                     .forEach(r -> this.writeExcelRow(r,sheet, rowCount.incrementAndGet()));
         }
-        excelOut = new FileOutputStream(excelFileName);
+        historicOut = new FileOutputStream(historicFileName);
 
-        workbook.write(excelOut);
-        excelOut.close();
+        workbook.write(historicOut);
+        historicOut.close();
         workbook.close();
         System.out.println("Excel file has been generated successfully.");
     }
 
-    private void writeExcelRow(MonthlyWeatherData monthData, HSSFSheet sheet, int rowNumber) {
-        HSSFRow row = sheet.createRow((short) rowNumber);
+    private void writeExcelRow(MonthlyWeatherData monthData, HSSFSheet sheet, int cellNumber) {
+        HSSFRow row = sheet.createRow((short) cellNumber);
         createCell(row,0,monthData.getStationName());
         createCell(row,1,monthData.getMonthStartDate().format(YYYY_MM));
         createCell(row,2,monthData.getTempMinC());
@@ -168,26 +80,26 @@ public class MetoExcelWriter {
         createCell(row,6,monthData.getSunHours());
 
     }
-    private void createCell(HSSFRow row, int rowNumber, String value) {
+    private void createCell(HSSFRow row, int column, String value) {
         if (value!=null) {
-            row.createCell(rowNumber).setCellValue(value);
+            row.createCell(column).setCellValue(value);
         } else {
-            row.createCell(rowNumber).setBlank();
+            row.createCell(column).setBlank();
         }
     }
-    private void createCell(HSSFRow row, int rowNumber, Float value) {
+    private void createCell(HSSFRow row, int column, Float value) {
         if (value!=null) {
-            row.createCell(rowNumber).setCellValue(DF.format(value));
+            row.createCell(column).setCellValue(DF.format(value));
         } else {
-            row.createCell(rowNumber).setBlank();
+            row.createCell(column).setBlank();
         }
     }
 
-    private void createCell(HSSFRow row, int rowNumber, Integer value) {
+    private void createCell(HSSFRow row, int column, Integer value) {
         if (value!=null) {
-            row.createCell(rowNumber).setCellValue(value);
+            row.createCell(column).setCellValue(value);
         } else {
-            row.createCell(rowNumber).setBlank();
+            row.createCell(column).setBlank();
         }
     }
 
